bccc4081565e0d15583df8dcd5ecbc850fead4b0
hani6148 <hanikhellafi@gmail.com>
2019-09-25 00:05:36 -0500

    Updates

---
+++ precache-manifest.03d3d305fcb0de47d6b46b3ccb88e679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341ef36661039691a9a80f379b7aaf98",
+    "url": "/react/./index.html"
+  },
+  {
+    "revision": "00a143109b0ff163c4b9",
+    "url": "/react/./static/css/main.cacfcdc5.chunk.css"
+  },
+  {
+    "revision": "6ad41db2a2c901a7a4df",
+    "url": "/react/./static/js/2.1ce9be25.chunk.js"
+  },
+  {
+    "revision": "00a143109b0ff163c4b9",
+    "url": "/react/./static/js/main.ccd14bcc.chunk.js"
+  },
+  {
+    "revision": "605451b7248934b4d8b7",
+    "url": "/react/./static/js/runtime~main.95d75e81.js"
+  }
+]);
